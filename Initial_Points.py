b3804945e9187161f19f7fc1c015f0b284eae05d
Saumya Sarawagi <sarawagisaumya@gmail.com>
2022-12-12 12:06:02 +0100

    Final_Part1

--- Initial_Points.py
+++ Initial_Points.py
@@ -87,7 +87,7 @@ Velocities=(10,12,15,17,20,23,25,27,30,32,35)
 
 X_initial = np.zeros((len(Velocities),17))
 #compaare objective function value and constraints
-#X_initial = np.load("Initial.npy")
+X_initial = np.load("Initial.npy")
 
 
 # x =[alpha, p, q, r, phi, theta, delta_a, delta_e, delta_r, delta_i] where delta i has 8 elelments
@@ -101,24 +101,25 @@ bnds_eng = ((ThrottleMin, ThrottleMax), (ThrottleMin, ThrottleMax))
 for i in range(int(g.N_eng / 2)):
     bnds = bnds + bnds_eng
 
-"""P = np.zeros((len(Velocities),1))
+P = np.zeros((len(Velocities),1))
 for i in range(len(Velocities)):
     V = Velocities[i]
     p = opt.opt_call(g, V, beta, gamma, R, Mach, Velocity, X_initial[i], bnds)
     P[i] = p
 
 plt.title("Power Consumption Curve")
-plt.xlabel("Power Consumed (W)")
-plt.ylabel("Velocity (m/s)")
+plt.ylabel("Power Consumed (W)")
+plt.xlabel("Velocity (m/s)")
 plt.plot(Velocities,P)
-plt.show()"""
+plt.grid()
+plt.show()
 
     
-for i in range(len(Velocities)):
+"""for i in range(len(Velocities)):
     V = Velocities[i]
     x0 = opt.opt_call(g, V, beta, gamma, R, Mach, Velocity, x0, bnds)
     X_initial[i,:] = x0
-np.save("Initial",X_initial)
+np.save("Initial",X_initial)"""
 
 
 
